1f58fbc3c31586a0a855bcb760d0b454591522b8
Hristo Panev <hristopanev20@gmail.com>
2025-09-06 02:46:26 +0300

    CORS-allow-everything

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: '*',
+    allowedHeaders: '*',
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
   await app.listen(3000);
