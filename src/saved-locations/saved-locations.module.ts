6ee3a436a1320af4a21e9992a24203ab9ff6b07a
Hristo Panev <hristopanev20@gmail.com>
2025-09-07 10:04:17 +0300

    import-User-to_location_module

--- src/saved-locations/saved-locations.module.ts
+++ src/saved-locations/saved-locations.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SavedLocation } from './saved-location.entity';
 import { SavedLocationsService } from './saved-locations.service';
 import { SavedLocationsController } from './saved-locations.controller';
+import { User } from 'src/users/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([SavedLocation])],
+  imports: [TypeOrmModule.forFeature([SavedLocation, User])],
   providers: [SavedLocationsService],
   controllers: [SavedLocationsController],
 })
